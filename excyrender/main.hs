962d0280051704830590977925f460e44fdfa386
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-02-19 17:54:10 +0100

    add dummy main.hs

---
+++ excyrender/main.hs
@@ -0,0 +1,7 @@
+-- (C) 2013 Sebastian Mach (1983), this file is published under the terms of the
+-- GNU General Public License, Version 3 (a.k.a. GPLv3).
+-- See COPYING in the root-folder of the excygen project folder.
+
+import Geometry
+
+main = putStrLn $ show (normal 1 1 1)
