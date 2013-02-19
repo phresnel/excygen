7db7f4cc009ed1c0a72fb06421ac307dc7a8e27e
Sebastian Mach <phresnel@gmail.com>
2013-02-19 07:01:47 +0100

    add type Vector

--- Geometry.hs
+++ Geometry.hs
@@ -12,3 +12,13 @@ degrees (Radians val) = val * (180/pi)
 radians :: (Floating t) => Angle t -> t
 radians (Degrees val) = val * (pi/180)
 radians (Radians val) = val
+
+-- Vector ----------------------------------------------------------------------
+data Vector t = Vector t t t
+                deriving (Show)
+
+v_add :: (Num t) => Vector t -> Vector t -> Vector t
+v_sub :: (Num t) => Vector t -> Vector t -> Vector t
+
+v_add (Vector a b c) (Vector x y z) = Vector (a+x) (b+y) (c+z) 
+v_sub (Vector a b c) (Vector x y z) = Vector (a-x) (b-y) (c-z) 
