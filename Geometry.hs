5964b324a35f8a71a6c9fefb3c8af7540c5cc22b
Sebastian Mach <phresnel@gmail.com>
2013-02-19 07:45:48 +0100

    add v_len, v_len_sq

--- Geometry.hs
+++ Geometry.hs
@@ -25,19 +25,23 @@ as_radians angle = Radians $ radians angle
 data Vector t = Vector t t t
                 deriving (Show)
 
-v_add :: (Num t) => Vector t -> Vector t -> Vector t
-v_sub :: (Num t) => Vector t -> Vector t -> Vector t
+v_add       :: (Num t)      => Vector t -> Vector t -> Vector t
+v_sub       :: (Num t)      => Vector t -> Vector t -> Vector t
+v_len_sq    :: (Num t)      => Vector t -> t
+v_len       :: (Floating t) => Vector t -> t
 
-v_add (Vector a b c) (Vector x y z) = Vector (a+x) (b+y) (c+z) 
-v_sub (Vector a b c) (Vector x y z) = Vector (a-x) (b-y) (c-z) 
+v_add       (Vector a b c) (Vector x y z) = Vector (a+x) (b+y) (c+z) 
+v_sub       (Vector a b c) (Vector x y z) = Vector (a-x) (b-y) (c-z) 
+v_len_sq    (Vector a b c)                = a*a + b*b + c*c
+v_len                                     = sqrt . v_len_sq
 
 -- Point -----------------------------------------------------------------------
 data Point t = Point t t t
                deriving (Show)
 
-p_add  :: (Num t) => Point t -> Vector t -> Point t
-p_sub  :: (Num t) => Point t -> Vector t -> Point t
-p_diff :: (Num t) => Point t -> Point t  -> Vector t
+p_add  :: (Num t) => Point t -> Vector t -> Point  t
+p_sub  :: (Num t) => Point t -> Vector t -> Point  t
+p_diff :: (Num t) => Point t -> Point  t -> Vector t
 
 p_add  (Point a b c) (Vector x y z) = Point  (a+x) (b+y) (c+z)
 p_sub  (Point a b c) (Vector x y z) = Point  (a-x) (b-y) (c-z)
