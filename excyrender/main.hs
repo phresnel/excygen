8ac2faeb17fcad85a25d3bba9591e94ed7858895
Sebastian Mach <phresnel@gmail.com>
2013-02-28 07:09:15 +0100

    add rendering function mockup

--- excyrender/main.hs
+++ excyrender/main.hs
@@ -10,10 +10,27 @@ import Sphere
 
 data ImageSize = ImageSize { width :: Int, height :: Int }
 
-raytracePixel :: (Fractional t, Shape s) => s t -> t -> t -> RGB t
-raytracePixel shape u v = RGB u v 0
+data Radiance t = Radiance t t t
+radianceFromRGB :: RGB t -> Radiance t
+radianceFromRGB (RGB r g b) = Radiance r g b 
+radianceToRGB :: Radiance t -> RGB t
+radianceToRGB (Radiance r g b) = RGB r g b
 
-raytraceImage :: (Fractional t, Shape s) => ImageSize -> s t -> [RGB t]
+-- rendering function
+recursive :: (Fractional t, Shape s) => s t -> Ray t -> Radiance t
+recursive shape ray = 
+    let d = ray_direction ray
+    in radianceFromRGB $ RGB (d_u d) (d_v d) (d_w d)  
+
+-- function that creates a primary ray and calls the rendering function
+raytracePixel :: (Floating t, Shape s) => s t -> t -> t -> RGB t
+raytracePixel shape u v = 
+    let ray = Geometry.ray (Point 0 0 0) (direction u v 1)
+        radiance = recursive shape ray
+    in radianceToRGB radiance
+
+-- constructs an image by raytracing over all pixels
+raytraceImage :: (Floating t, Shape s) => ImageSize -> s t -> [RGB t]
 raytraceImage size shape = [let u = fromIntegral(x) / fromIntegral(width size)
                                 v = fromIntegral(y) / fromIntegral(height size)
                             in raytracePixel shape u v
