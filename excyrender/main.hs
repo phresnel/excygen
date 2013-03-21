a6d352f1cf06ca30d305898cc34332d5cd64b195
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-03-21 22:20:40 +0100

    add surface-integrator 'primary'

--- excyrender/main.hs
+++ excyrender/main.hs
@@ -2,15 +2,27 @@
 -- GNU General Public License, Version 3 (a.k.a. GPLv3).
 -- See COPYING in the root-folder of the excygen project folder.
 
-import Geometry(Ray(..),Point(..),direction)
+import Geometry(Ray(..),ray_direction,Point(..),direction,d_u,d_v)
 import PPM(toPPM)
 import RGB
 import Shape
 import Sphere
 import Intersection
 
-raytrace :: (RealFrac t, Floating t, Shape s) => Int -> Int -> s t -> [RGB t]
-raytrace width height shape =
+-- integrator for only primary intersections -----------------------------------
+primary :: (RealFrac t, Floating t, Shape s) => Ray t -> s t -> RGB t
+primary ray shape =
+    let intersection = intersect ray shape
+    in case intersection of
+       Just i -> diffuse i
+       Nothing -> let dir = ray_direction ray
+                  in RGB (d_u dir) (d_v dir) 0
+
+
+-- simple renderer -------------------------------------------------------------
+raytrace :: (RealFrac t, Floating t, Shape s)
+    => Int -> Int -> s t -> (Ray t -> s t -> RGB t) -> [RGB t]
+raytrace width height shape surface_integrator =
     -- TODO just do a list comprehension?
     let raytrace_rows y | y==height = []
                         | otherwise = scanline 0 y ++ raytrace_rows (y+1)
@@ -20,10 +32,7 @@ raytrace width height shape =
             let u = fromIntegral(x) / fromIntegral(width)
                 v = 1 - fromIntegral(y) / fromIntegral(height)
                 ray = Ray (Point 0 0 0) (direction (u-0.5) (v-0.5) 1)
-                intersection = intersect ray shape
-            in case intersection of
-               Just i -> diffuse i
-               Nothing -> RGB u v 0
+            in surface_integrator ray shape
     in raytrace_rows 0
 
 
@@ -31,7 +40,7 @@ ppm =
   let width  = 64
       height = 64
       shape  = sphere (Point 0 0 5) 1
-      pixels = raytrace width height shape
+      pixels = raytrace width height shape primary
   in  toPPM width height pixels
 
 main = putStrLn $ ppm
