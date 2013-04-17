2f3f0267b21ab23b2a057df98d0b8a6520366da1
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-04-17 08:24:50 +0200

    add XYZ -> sRGB conversion

---
+++ excyrender/ColorSpace.hs
@@ -0,0 +1,15 @@
+-- (C) 2013 Sebastian Mach (1983), this file is published under the terms of the
+-- GNU General Public License, Version 3 (a.k.a. GPLv3).
+-- See COPYING in the root-folder of the excygen project folder.
+
+module ColorSpace
+where
+
+
+from_XYZ_to_sRGB :: (RealFrac a) => (a, a, a) -> (a, a, a)
+
+
+from_XYZ_to_sRGB (x, y, z) =
+    ( 3.240479*x - 1.537150*y - 0.498535*z,
+     -0.969256*x + 1.875991*y + 0.041556*z,
+      0.055648*x - 0.204043*y + 1.057311*z)
