d45b67f93ef9036654d6a5be5c1c9307070f22a7
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-07-09 06:53:34 +0200

    add license

--- excyrender.cxx/ImageFormat/PPM.hh
+++ excyrender.cxx/ImageFormat/PPM.hh
@@ -1,3 +1,6 @@
+// (C) 2013 Sebastian Mach (1983), this file is published under the terms of the
+// GNU General Public License, Version 3 (a.k.a. GPLv3).
+// See COPYING in the root-folder of the excygen project folder.
 #ifndef PPM_HH_INCLUDED_20130708
 #define PPM_HH_INCLUDED_20130708
 
