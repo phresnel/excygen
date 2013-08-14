cafbe02a9d0d598b3455c80444fe2978f52716c1
Sebastian Mach / phresnel <phresnel@gmail.com>
2013-08-14 09:55:16 +0200

    stub for function_call

--- excyrender.cxx/Nature/Et1.cc
+++ excyrender.cxx/Nature/Et1.cc
@@ -4,35 +4,60 @@
 
 #include "Et1.hh"
 #include "optional.hh"
+#include "memory.hh"
 #include <stdexcept>
 #include <vector>
+#include <algorithm>
 
+
+// -- Tokenization ---------------------------------------------------------------------------------
 namespace excyrender { namespace Nature { namespace Et1 { namespace {
 
     using string = std::string;
     using iterator = string::const_iterator;
     using std::vector;
 
-
-    // -- Token ------------------------------------------------------------------------------------
     enum TokenKind {
         Integer,
-        Operator,
-        Identifier
+        Identifier,
+        LParen, RParen,
+        Plus, Minus,
+        Asterisk, Slash,
+        Comma
     };
 
     struct Token {
         TokenKind kind;
         iterator from, to;
 
+        bool operator== (const char *c_str) const {
+            return string(from,to) == c_str;
+        }
+
+        bool operator!= (const char *c_str) const {
+            return !(*this == c_str);
+        }
+
+        operator string() const {
+            return string(from,to);
+        }
+
         Token (TokenKind kind, iterator from, iterator to) : kind(kind), from(from), to(to) {}
     };
+
+    using token_iter = vector<Token>::const_iterator;
 
 
     std::ostream& operator<< (std::ostream &os, Token const &tok) {
         switch (tok.kind) {
         case Integer: os << "[int:"; break;
-        case Operator: os << "["; break;
+        case LParen: os << "'('"; break;
+        case RParen: os << "')'"; break;
+        case Plus: os << "'+'"; break;
+        case Minus: os << "'-'"; break;
+        case Asterisk: os << "'*'"; break;
+        case Slash: os << "'/'"; break;
+        case Comma: os << "','"; break;
         case Identifier: os << "[id:"; break;
         }
         return os << string(tok.from, tok.to) << "] ";
@@ -44,6 +69,8 @@ namespace excyrender { namespace Nature { namespace Et1 { namespace {
         }
         return os;
     }
+
+
 
     // -- Tokenizer Helpers ------------------------------------------------------------------------
     namespace detail {
@@ -99,20 +126,6 @@ namespace excyrender { namespace Nature { namespace Et1 { namespace {
             return it;
         }
 
-        optional<iterator> operator_(iterator it, iterator end)
-        {
-            if (*it == '+'
-                    || *it == '-'
-                    || *it == '*'
-                    || *it == '/'
-                    || *it == '(' || *it == ')'
-                    )
-            {
-                return ++it;
-            }
-            return optional<iterator>();
-        }
-
         optional<iterator> identifier(iterator it, iterator end) {
             if (!is_letter(*it))
                 return optional<iterator>();
@@ -135,9 +148,31 @@ namespace excyrender { namespace Nature { namespace Et1 { namespace {
             if (auto oit = integer(it, end)) {
                 tokens.emplace_back(Integer, it, *oit);
                 it = *oit;
-            } else if (auto oit = operator_(it, end)) {
+            } /* else if (auto oit = operator_(it, end)) {
                 tokens.emplace_back(Operator, it, *oit);
                 it = *oit;
+            }*/
+            else if (*it == '(') {
+                tokens.emplace_back(LParen, it, it+1);
+                ++it;
+            } else if (*it == ')') {
+                tokens.emplace_back(RParen, it, it+1);
+                ++it;
+            } else if (*it == '+') {
+                tokens.emplace_back(Plus, it, it+1);
+                ++it;
+            } else if (*it == '-') {
+                tokens.emplace_back(Minus, it, it+1);
+                ++it;
+            } else if (*it == '*') {
+                tokens.emplace_back(Asterisk, it, it+1);
+                ++it;
+            } else if (*it == '/') {
+                tokens.emplace_back(Slash, it, it+1);
+                ++it;
+            } else if (*it == ',') {
+                tokens.emplace_back(Comma, it, it+1);
+                ++it;
             } else if (auto oit = identifier(it, end)) {
                 tokens.emplace_back(Identifier, it, *oit);
                 it = *oit;
@@ -151,12 +186,108 @@ namespace excyrender { namespace Nature { namespace Et1 { namespace {
 } } } }
 
 
+
+// -- Compilation ----------------------------------------------------------------------------------
+namespace excyrender { namespace Nature { namespace Et1 { namespace {
+
+    class ASTNode {
+    public:
+        ASTNode() = delete;
+        virtual ~ASTNode() = 0;
+
+        token_iter from() const noexcept { return from_; }
+        token_iter to()   const noexcept { return to_; }
+
+    protected:
+        ASTNode(token_iter from, token_iter to) : from_(from), to_(to) {}
+
+    private:
+        token_iter from_, to_;
+    };
+
+
+
+    optional<Token> lookahead(token_iter it, token_iter end)  {
+        if (it != end && it+1 != end)
+            return *it;
+        return optional<Token>();
+    }
+
+    bool lookahead_compare(token_iter it, token_iter end, TokenKind kind)  {
+        if (auto x = lookahead(it, end))
+            return x->kind == kind;
+        return false;
+    }
+
+    unique_ptr<ASTNode> expression(token_iter, token_iter) {
+        std::cerr << "expression() not implemented" << std::endl;
+        return unique_ptr<ASTNode>();
+    }
+
+    // function-call : identifier '(' expression (',' expression)* ')'
+    bool function_call(token_iter it, token_iter end)
+    {
+        // Name.
+        if (it->kind != Identifier)
+            return false;
+        const string callee = *it;
+        ++it;
+
+        // Open Paren.
+        if (it == end || it->kind != LParen)
+            return false;
+        ++it;
+
+        // Arguments.
+        std::cout << "looks like a call to '" << callee << "'" << std::endl;
+
+        while (it != end) {
+            const auto node = expression(it, end);
+            if (!node) {
+                if (it->kind == RParen)
+                    break;
+                throw std::runtime_error("expected ')'");
+            }
+            it = node->to();
+            if (it==end || it->kind!=Comma)
+                throw std::runtime_error("expected ','");
+            ++it;
+
+            /* PUSH NODE TO ARGUMENT LIST */
+        }
+        if (it == end || it->kind != RParen)
+            throw std::runtime_error("unclosed '(' in function call");
+
+        return false;
+    }
+
+    // factor
+    //     : function-call
+    //     | scalar
+    //     | '(' expression ')'
+    /*token_iter factor(token_iter it, token_iter end) {
+        if (function_call
+        //return it;
+    }*/
+
+    HeightFunction compile (vector<Token> const &toks) {
+        if (toks.empty())
+            throw std::runtime_error("no tokens");
+        if (function_call(toks.begin(), toks.end())) {
+            std::cout << "fun-call found" << std::endl;
+        }
+        throw std::runtime_error("not implemented");
+    }
+
+} } } }
+
+
+
+// -- API ------------------------------------------------------------------------------------------
 namespace excyrender { namespace Nature { namespace Et1 {
 
 HeightFunction compile (std::string const &code) {
-    const auto tokens = tokenize(code);
-    std::cout << tokens << '\n';
-    throw std::runtime_error("Et1::compile() not fully implemented");
+    return compile(tokenize(code));
 }
 
 } } }
